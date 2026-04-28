71c9203e4bcab830498cd2679fcbe19ff2d4fd82
Julia <julia.bonowicz2@gmail.com>
2026-04-28 14:03:01 +0200

    Add files via upload

---
+++ 02_product_revenue.sql
@@ -0,0 +1,9 @@
+WITH calculations AS (SELECT YEAR(o.orderDate) AS year ,MONTH(o.orderDate) AS month, 
+SUM(ROUND((od.quantityOrdered*od.priceEach),2)) AS monthly_revenue
+FROM orders o
+LEFT JOIN orderdetails od
+ON od.orderNumber=o.orderNumber
+GROUP BY year,month)
+SELECT year,month,monthly_revenue, LAG(monthly_revenue) OVER (ORDER BY year,month) AS previous_month_revenue, 
+monthly_revenue - LAG(monthly_revenue) OVER (ORDER BY year,month) AS revenue_change
+FROM calculations
